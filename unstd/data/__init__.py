5a194914b6e74680aa3e610c74b820c7e9d3b6ca
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2024-08-14 23:58:13 +0000

    list data structure

---
+++ unstd/data/__init__.py
@@ -0,0 +1,2 @@
+from unstd.data.__internal import *
+from unstd.data.list import List
---
+++ unstd/data/__internal.py
@@ -0,0 +1,5 @@
+import typing
+Optional = typing.Optional
+Callable = typing.Callable
+Iterable = typing.Iterable
+Any = typing.Any
